10e932d76758bef502d855a41511439057c2fdb7
weiwei.zhang <weiwei.zhang@soulinfo.com>
2015-04-28 13:40:23 +0800

    edit common.java

--- YB_Books/src/com/soul/data/Common.java
+++ YB_Books/src/com/soul/data/Common.java
@@ -9,15 +9,11 @@ public class Common {
 	public static final String FILES_PATH = "files";
 
 	public static final String YB_MESSAGE_PATH = FILES_PATH + "/YB_message.xls";
-	public static final String UP_FILE_EXE = FILES_PATH + "upfile.exe";
 	
 	public static final String BASE_URL = "http://192.168.2.185";
 	
-	
-	public static final String CLASS_NAME = "soulTest400";
-//	public static final String YB_LOGIN_PATH = FILES_PATH + "/YB_login.xls";
-//	public static final String YB_VOTE_PATH = FILES_PATH + "/YB_vote.xls";
-//	public static final String YB_SAY_PATH = FILES_PATH + "/YB_say.xls";
-//	public static final String YB_MESSAGE_PATH = FILES_PATH + "/YB_message.xls";
+	public static final String CLASS_NAME = "soulTest451";
+
+//	public static final String UP_FILE_EXE = FILES_PATH + "upfile.exe";
 
 }
